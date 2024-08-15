0cf2c70f14aa2313cc5f9f95129c86d31bc4c16c
loki <lokiapm888@gmail.com>
2024-08-15 13:34:21 +0530

    initial

---
+++ target/debug/deps/channels-cdc28251e3568e61.d
@@ -0,0 +1,7 @@
+/home/loki/programs/rust/h-projects/channels/target/debug/deps/libchannels-cdc28251e3568e61.rmeta: src/main.rs src/channel.rs src/dequeue.rs
+
+/home/loki/programs/rust/h-projects/channels/target/debug/deps/channels-cdc28251e3568e61.d: src/main.rs src/channel.rs src/dequeue.rs
+
+src/main.rs:
+src/channel.rs:
+src/dequeue.rs:
---
+++ target/debug/deps/channels-dc3966c17e36a936.d
@@ -0,0 +1,7 @@
+/home/loki/programs/rust/h-projects/channels/target/debug/deps/libchannels-dc3966c17e36a936.rmeta: src/main.rs src/channel.rs src/dequeue.rs
+
+/home/loki/programs/rust/h-projects/channels/target/debug/deps/channels-dc3966c17e36a936.d: src/main.rs src/channel.rs src/dequeue.rs
+
+src/main.rs:
+src/channel.rs:
+src/dequeue.rs:
